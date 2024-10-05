d74fe4d2349101b3f7441f56d4b889343691b314
thereisnothinginside <108665333+thereisnothinginside@users.noreply.github.com>
2024-10-05 17:51:59 +0300

    gün4

---
+++ gün4/widget 1/widget 1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace widget_1
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Saat güncelleyici zamanlayıcıyı başlat
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateClock;
+            timer.Start();
+
+            // Saatin ilk güncellenmesini tetikle
+            UpdateClock(null, null);
+        }
+
+        private void UpdateClock(object sender, EventArgs e)
+        {
+            // Saati TextBlock'a yaz
+            ClockText.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
